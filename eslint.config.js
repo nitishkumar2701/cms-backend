86e71cd963e49a10ae3724d3cfa116a0fb657776
Nitish <nk61910@gmail.com>
2026-09-15 13:37:01 +0100

    generic keyword lint issue fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
-// eslint.config.js
 module.exports = [
+  {
+    // Ignore frontend browser scripts from backend linting rules
+    ignores: ["src/public/**"],
+  },
   {
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "commonjs"
+      sourceType: "commonjs",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        setTimeout: "readonly"
+      }
     },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "no-undef": "error"
     }
   }
