f7a1d750bd85d344e1e33b9a737b8ada193f2ee2
Nitish <nk61910@gmail.com>
2026-09-15 13:31:29 +0100

    es-lint issue fix

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
